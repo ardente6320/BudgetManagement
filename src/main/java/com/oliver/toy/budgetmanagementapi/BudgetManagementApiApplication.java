c01a22be3347439155e30c678235ff3ab645ebf3
oliver <ardente6320@gmail.com>
2022-05-14 14:48:57 +0900

    project init

---
+++ src/main/java/com/oliver/toy/budgetmanagementapi/BudgetManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.oliver.toy.budgetmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliver/toy/budgetmanagementapi/BudgetManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliver.toy.budgetmanagementapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
